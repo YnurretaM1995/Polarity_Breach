437d0f92980c1eea334c116cf5a85754904e9276
Mario Ynurreta <ynurretam@gmail.com>
2026-07-03 17:11:46 -0400

    Damage if hit the player

--- Assets/Scripts/Enemy/Projectile.cs
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -1,43 +1,57 @@
-using UnityEngine;
+using PolarityBreach.PolaritySystem;
 using PolarityBreach.PolaritySystem.Interfaces;
+using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class Projectile : MonoBehaviour
+namespace PolarityBreach.Enemy
 {
-    [SerializeField] private float lifetime = 4f;
-
-    private Rigidbody rb;
-    private float speed;
-    private float damage;
-
-    private void Awake()
+    [RequireComponent(typeof(Rigidbody))]
+    public class Projectile : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
-    }
+        [SerializeField] private float lifetime = 4f;
 
-    public void Launch(Vector3 direction, float launchSpeed, float launchDamage)
-    {
-        speed = launchSpeed;
-        damage = launchDamage;
-        rb.linearVelocity = direction.normalized * speed;
-        Destroy(gameObject, lifetime);
-    }
+        private Rigidbody rb;
+        private float speed;
+        private float damage;
+        private PolarityComponent _polarity;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Enemy")) return;
-        IDamageable damageable = other.GetComponent<IDamageable>();
-        if (damageable != null)
+        private void Awake()
         {
-            damageable.TakeDamage(damage);
-            Destroy(gameObject);
-            return;
+            rb = GetComponent<Rigidbody>();
+            _polarity = GetComponent<PolarityComponent>();
+            rb.useGravity = false;
         }
-        
-        if (!other.isTrigger)
+
+        public void Launch(Vector3 direction, float launchSpeed, float launchDamage)
         {
-            Destroy(gameObject);
+            speed = launchSpeed;
+            damage = launchDamage;
+            rb.linearVelocity = direction.normalized * speed;
+            Destroy(gameObject, lifetime);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+            if (other.CompareTag("Enemy")) return;
+            if (!other.CompareTag("Player")) return;
+            bool hit = DamageSystem.TryApplyDamage(_polarity, other.gameObject, damage);
+            
+            if (hit)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            
+            IPolarizable otherPolarity = other.GetComponent<IPolarizable>();
+            if (otherPolarity != null)
+            {
+                return; 
+            }
+        
+            if (!other.isTrigger)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
