e97ce64ef457edffe7ce64032a2d24b75f0b5a53
Mario Ynurreta <ynurretam@gmail.com>
2026-07-02 20:10:35 -0400

    Pause with xbox controlles

--- Assets/Scripts/Player/PauseMenu.cs
+++ Assets/Scripts/Player/PauseMenu.cs
@@ -1,36 +1,53 @@
 using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PauseMenu : MonoBehaviour
+namespace PolarityBreach.Player
 {
-    [SerializeField] private GameObject pausePanel;
-
-    public static bool IsPaused { get; private set; }
-    public static event Action<bool> OnPauseChanged; 
-
-    private void Update()
+    public class PauseMenu : MonoBehaviour
     {
-        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        [SerializeField] private GameObject pausePanel;
+
+        public static bool IsPaused { get; private set; }
+        public static event Action<bool> OnPauseChanged;
+
+        private PlayerInputActions controls;
+
+        private void Awake()
+        {
+            controls = new PlayerInputActions();
+            controls.Player.Pause.performed += ctx => TogglePause();
+        }
+
+        private void OnEnable()
+        {
+            controls.Player.Pause.Enable();
+        }
+
+        private void OnDisable()
+        {
+            controls.Player.Pause.Disable();
+        }
+
+        private void TogglePause()
         {
             if (IsPaused) Resume();
             else Pause();
         }
-    }
 
-    private void Pause()
-    {
-        IsPaused = true;
-        pausePanel.SetActive(true);
-        Time.timeScale = 0f;
-        OnPauseChanged?.Invoke(true);
-    }
+        private void Pause()
+        {
+            IsPaused = true;
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+            OnPauseChanged?.Invoke(true);
+        }
 
-    private void Resume()
-    {
-        IsPaused = false;
-        pausePanel.SetActive(false);
-        Time.timeScale = 1f;
-        OnPauseChanged?.Invoke(false);
+        private void Resume()
+        {
+            IsPaused = false;
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+            OnPauseChanged?.Invoke(false);
+        }
     }
 }
