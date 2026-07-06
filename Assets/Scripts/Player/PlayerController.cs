352609d9a43a77ea7e14d0c14de7c8592c1aeaa6
RobbieSR <robbiesutherlandrusso@gmail.com>
2026-07-06 17:40:32 -0400

    Refactored PlayerController to use PlayerStatsData

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,165 +1,166 @@
 using System.Collections;
-using PolarityBreach.Player;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
-public class PlayerController : MonoBehaviour
+namespace PolarityBreach.Player
 {
-    [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private float dashSpeed = 20f;
-    [SerializeField] private float dashDuration = 0.2f;
-    [SerializeField] private float dashCooldown = 1.0f;
-
-    private Rigidbody rb;
-    private Vector3 moveInput;
-    private Vector3 lookDirection;
-    private PlayerInputActions controls;
-    private bool isUsingGamepad = false;
-
-    private bool isDashing = false;
-    private bool canDash = true;
-    private Vector3 dashDirection;
-
-    private void Awake()
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : MonoBehaviour
     {
-        InitializeInputSystem();
-    }
+        private Rigidbody rb;
+        private Vector3 moveInput;
+        private Vector3 lookDirection;
+        private PlayerInputActions controls;
+        private bool isUsingGamepad = false;
 
-    private void OnEnable()
-    {
-        controls.Player.Enable();
-        PauseMenu.OnPauseChanged += HandlePause;
-    }
+        private bool isDashing = false;
+        private bool canDash = true;
+        private Vector3 dashDirection;
+        private PlayerStatsData _playerStats;
 
-    private void OnDisable()
-    {
-        controls.Player.Disable();
-        PauseMenu.OnPauseChanged -= HandlePause;
-    }
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-        RotatePlayerTowardAim();
-        if(isDashing) return;
-        ReadMovementInput();
-        CalculateAimDirection();
-        RotatePlayerTowardAim();
-    }
-
-    private void FixedUpdate()
-    {
-        if (isDashing)
+        private void Awake()
         {
-            ApplyDashPhysics();
-        }
-        else
-        {
-            ApplyMovementPhysics();
-        }
-    }
-    
-    private void HandlePause(bool paused)
-    {
-        if (paused) controls.Player.Disable();
-        else controls.Player.Enable();
-    }
-
-    private void InitializeInputSystem()
-    {
-        controls = new PlayerInputActions();
-        controls.Player.Dash.performed += ctx => TryTriggerDash();
-    }
-
-    private void ReadMovementInput()
-    {
-        Vector2 inputVector = controls.Player.Movement.ReadValue<Vector2>();
-        moveInput = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
-    }
-
-    private void CalculateAimDirection()
-    {
-        Vector2 mouseScreenPos = controls.Player.MouseAim.ReadValue<Vector2>();
-        Vector2 stickInput = controls.Player.GamepadAim.ReadValue<Vector2>();
-
-        if (stickInput.magnitude > 0.1f)
-        {
-            isUsingGamepad = true;
-        }
-        else if (Mouse.current != null && Mouse.current.delta.ReadValue().magnitude > 0.1f)
-        {
-            isUsingGamepad = false;
+            InitializeInputSystem();
+            _playerStats = GetComponent<PlayerStatsData>();
         }
 
-        if (isUsingGamepad)
+        private void OnEnable()
         {
-            if (stickInput.magnitude > 0.1f)
+            controls.Player.Enable();
+            PauseMenu.OnPauseChanged += HandlePause;
+        }
+
+        private void OnDisable()
+        {
+            controls.Player.Disable();
+            PauseMenu.OnPauseChanged -= HandlePause;
+        }
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            RotatePlayerTowardAim();
+            if(isDashing) return;
+            ReadMovementInput();
+            CalculateAimDirection();
+            RotatePlayerTowardAim();
+        }
+
+        private void FixedUpdate()
+        {
+            if (isDashing)
             {
-                lookDirection = new Vector3(stickInput.x, 0f, stickInput.y).normalized;
+                ApplyDashPhysics();
+            }
+            else
+            {
+                ApplyMovementPhysics();
             }
         }
-        else
+    
+        private void HandlePause(bool paused)
         {
-            Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(transform.position);
-
-            float deltaX = mouseScreenPos.x - playerScreenPos.x;
-            float deltaY = mouseScreenPos.y - playerScreenPos.y;
-
-            lookDirection = new Vector3(deltaX, 0f, deltaY).normalized;
-        }
-    }
-
-
-    private void RotatePlayerTowardAim()
-    {
-        if (lookDirection != Vector3.zero)
-        {
-            transform.forward = lookDirection;
-        }
-    }
-
-    private void ApplyMovementPhysics()
-    {
-        rb.linearVelocity = new Vector3(moveInput.x * moveSpeed, rb.linearVelocity.y, moveInput.z * moveSpeed);
-    }
-
-    private void ApplyDashPhysics()
-    {
-        rb.linearVelocity = new Vector3(dashDirection.x * dashSpeed, rb.linearVelocity.y, dashDirection.z * dashSpeed);
-    }
-
-    private void TryTriggerDash()
-    {
-        if (!canDash || isDashing) return;
-
-        if (moveInput != Vector3.zero)
-        {
-            dashDirection = moveInput;
-        }
-        else
-        {
-            dashDirection = transform.forward;
+            if (paused) controls.Player.Disable();
+            else controls.Player.Enable();
         }
 
-        StartCoroutine(DashCoroutine());
-    }
+        private void InitializeInputSystem()
+        {
+            controls = new PlayerInputActions();
+            controls.Player.Dash.performed += ctx => TryTriggerDash();
+        }
 
-    private IEnumerator DashCoroutine()
-    {
-        canDash = false;
-        isDashing = true;
-        yield return new WaitForSeconds(dashDuration);
+        private void ReadMovementInput()
+        {
+            Vector2 inputVector = controls.Player.Movement.ReadValue<Vector2>();
+            moveInput = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+        }
+
+        private void CalculateAimDirection()
+        {
+            Vector2 mouseScreenPos = controls.Player.MouseAim.ReadValue<Vector2>();
+            Vector2 stickInput = controls.Player.GamepadAim.ReadValue<Vector2>();
+
+            if (stickInput.magnitude > 0.1f)
+            {
+                isUsingGamepad = true;
+            }
+            else if (Mouse.current != null && Mouse.current.delta.ReadValue().magnitude > 0.1f)
+            {
+                isUsingGamepad = false;
+            }
+
+            if (isUsingGamepad)
+            {
+                if (stickInput.magnitude > 0.1f)
+                {
+                    lookDirection = new Vector3(stickInput.x, 0f, stickInput.y).normalized;
+                }
+            }
+            else
+            {
+                Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+                float deltaX = mouseScreenPos.x - playerScreenPos.x;
+                float deltaY = mouseScreenPos.y - playerScreenPos.y;
+
+                lookDirection = new Vector3(deltaX, 0f, deltaY).normalized;
+            }
+        }
+
+
+        private void RotatePlayerTowardAim()
+        {
+            if (lookDirection != Vector3.zero)
+            {
+                transform.forward = lookDirection;
+            }
+        }
+
+        private void ApplyMovementPhysics()
+        {
+            rb.linearVelocity = new Vector3(moveInput.x * _playerStats.movementSpeed, rb.linearVelocity.y, moveInput.z * _playerStats.movementSpeed);
+        }
+
+        private void ApplyDashPhysics()
+        {
+            rb.linearVelocity = new Vector3(dashDirection.x * _playerStats.dashSpeed, rb.linearVelocity.y, dashDirection.z * _playerStats.dashSpeed);
+        }
+
+        private void TryTriggerDash()
+        {
+            if (!_playerStats.dashUnlocked) return;
+            
+            if (!canDash || isDashing) return;
+            
+            if (moveInput != Vector3.zero)
+            {
+                dashDirection = moveInput;
+            }
+            else
+            {
+                dashDirection = transform.forward;
+            }
+
+            StartCoroutine(DashCoroutine());
+        }
+
+        private IEnumerator DashCoroutine()
+        {
+            canDash = false;
+            isDashing = true;
+            yield return new WaitForSeconds(_playerStats.dashDuration);
         
-        isDashing = false;
+            isDashing = false;
         
-        yield return new WaitForSeconds(dashCooldown);
+            yield return new WaitForSeconds(_playerStats.dashCooldown);
         
-        canDash = true;
-    }
+            canDash = true;
+        }
 
+    }
 }
