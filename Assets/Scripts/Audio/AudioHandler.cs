1c751d4f947359df2c60455654e822721deab3aa
RobbieSR <robbiesutherlandrusso@gmail.com>
2026-08-10 19:32:40 -0400

    Added code guard if audio clip == null

--- Assets/Scripts/Audio/AudioHandler.cs
+++ Assets/Scripts/Audio/AudioHandler.cs
@@ -6,6 +6,8 @@ namespace PolarityBreach.Audio
     {
         public static void Play3DSound(AudioClip clip, Vector3 position)
         {
+            if (clip == null) return;
+
             AudioSource.PlayClipAtPoint(clip, position);
         }
     }
